23757b5a19e6c3eb2a5d562d61459de153d4a82d
SandroVelhinoCasa <pocascocas@gmail.com>
2024-07-03 11:55:39 +0100

    sdfsf

--- back/services/userthings.js
+++ back/services/userthings.js
@@ -5,21 +5,38 @@ require("dotenv").config();
 const uri = process.env.uri;
 
 const UserSchema = require("../models/userModel");
+const { isEmail } = require("validator");
 
 const signin = async (req, res) => {
   let { body } = req;
   try {
     await mongoose.connect(uri);
-
     const newUser = new UserSchema(body);
     await newUser.save();
+
     console.log(newUser);
-    res.send(newUser);
+    if (newUser) {
+      res.status(200).send(true);
+    }
   } catch (e) {
     console.log("singin function catched: ", e);
+    let objeto = bodyErrorIdentify(body);
+    res.status(405).send(objeto);
   } finally {
     mongoose.connection.close();
   }
+};
+//TODO - funcção de login com token jwt
+
+const bodyErrorIdentify = (body) => {
+  if (isEmail(body.email) === false) {
+    body.email = false;
+  }
+  //FIXME - error andler de password dont work
+  if (body.password.lenght < 6) {
+    body.password = false;
+  }
+  return body;
 };
 
 module.exports = { signin };
